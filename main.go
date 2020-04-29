fb59de25edb403fe96efb1902b413fa123bdd4f9
Jason Nochlin <hundredwatt@gmail.com>
2020-04-28 21:19:49 -0400

    Implement dump entire table to csv for extract command

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
+	"io/ioutil"
 	"log"
 
+	"github.com/jimsmart/schema"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/xo/dburl"
 
@@ -24,5 +28,57 @@ func extract(source string, table string) {
 	database, err := dburl.Open(url)
 	if err != nil {
 		log.Fatal("Database Open Error:", err)
+	}
+
+	tables, err := schema.TableNames(database)
+	if err != nil {
+		log.Fatal(err)
+	}
+	targetTable := ""
+	for _, tablename := range tables {
+		if tablename == table {
+			targetTable = tablename
+		}
+	}
+	if targetTable == "" {
+		log.Fatalf("table \"%s\" not found in \"%s\"", table, source)
+	}
+
+	tmpfile, err := ioutil.TempFile("/tmp/", fmt.Sprintf("extract-%s-%s", table, source))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err := database.Query(fmt.Sprintf("SELECT * FROM %s", targetTable))
+	writer := csv.NewWriter(tmpfile)
+	columnNames, err := rows.Columns()
+	if err != nil {
+		log.Fatal(err)
+	}
+	writer.Write(columnNames)
+
+	rawResult := make([]interface{}, len(columnNames))
+	writeBuffer := make([]string, len(columnNames))
+	for i := range writeBuffer {
+		rawResult[i] = &writeBuffer[i]
+	}
+
+	for rows.Next() {
+		err := rows.Scan(rawResult...)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = writer.Write(writeBuffer)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	//defer os.Remove(tmpfile.Name())
+	writer.Flush()
+
+	if err := tmpfile.Close(); err != nil {
+		log.Fatal(err)
 	}
 }
